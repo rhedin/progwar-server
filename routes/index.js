bb47f41fdf45792e6373de8896c4783c17aa4475
rhedin <cubsno1@gmail.com>
2017-08-20 23:01:36 +0000

    Now a file is successfully saved on the server.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var fileops = require('../utils/fileOperations');
 
 router.post('/gameToServer', function(req, resp, next) {
-    console.log(`Weeeee are hereeeeee!`);
-    resp.send('I got it.');
-});
-
-router.get('/gameToServer', function(req, resp, next) {
-    console.log(`found gameToServer with get`);
-    resp.send('I got it.');
+    console.log(req.body);
+    console.log(req.body.name);
+    fileops.saveFile({
+        name: req.body.name,
+        path: 'games',
+        text: req.body.text,
+    }, (err) => {
+        if (err) {
+            resp.json({
+                ok: false,
+                statusText: `err = ${err}`,
+            })
+        } else {
+            resp.json({
+                ok: true,
+            });
+        }
+    });
 });
 
 /* GET home page. */
