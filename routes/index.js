89a7b7ff1dcf9e7882c86a99c735d3696a11e6a0
rhedin <cubsno1@gmail.com>
2017-08-20 21:48:30 +0000

    Now have client and server communicating.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+router.post('/gameToServer', function(req, resp, next) {
+    console.log(`Weeeee are hereeeeee!`);
+    resp.send('I got it.');
+});
+
+router.get('/gameToServer', function(req, resp, next) {
+    console.log(`found gameToServer with get`);
+    resp.send('I got it.');
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(`In get /  --  index.js`);
   res.render('index', { title: 'Express' });
 });
 
